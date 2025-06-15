2bebbfc8ddf433f7cdaa2cdacf5ef209ca735213
Andrew Dunn <atd2005@gmail.com>
2025-06-15 12:45:20 -0500

    add pipeline

--- lib/mapsterious-stack.ts
+++ lib/mapsterious-stack.ts
@@ -1,16 +1,32 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const GITHUB_ACCT = "andrewtdunn";
+const GITHUB_REPO = "mapsterious";
+const GITHUB_BRANCH = "main";
+
+const connectionArn =
+  "arn:aws:codeconnections:us-east-1:637423577773:connection/cb4dc86f-4341-4834-a59b-049f0e496d07";
 
 export class MapsteriousStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MapsteriousQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      synth: new cdk.pipelines.CodeBuildStep("Synth", {
+        input: CodePipelineSource.connection(
+          `${GITHUB_ACCT}/${GITHUB_REPO}`,
+          GITHUB_BRANCH,
+          {
+            connectionArn,
+          }
+        ),
+        installCommands: ["npm install -g npm@latest"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+      crossAccountKeys: true,
+    });
   }
 }
